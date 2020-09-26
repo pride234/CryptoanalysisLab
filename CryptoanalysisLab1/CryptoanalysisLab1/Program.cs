26ed3ac58e50fbd00235c317017eb72fc4349cb2
pride234 <shinigami466914@gmail.com>
2020-09-26 23:52:08 +0300

    added function for reading data from *.csv file types

--- CryptoanalysisLab1/CryptoanalysisLab1/Program.cs
+++ CryptoanalysisLab1/CryptoanalysisLab1/Program.cs
@@ -3,14 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
 
 namespace CryptoanalysisLab1 {
     class Program {
         static void Main(string[] args) {
+
+            DoLab(3);
+            Console.ReadKey();
+
         }
 
-        void DoLab(int variant) {
+        static void DoLab(int variant) {
             
+            Excel A = new Excel(@"C:\Users\PRIDE\source\repos\CryptoanalysisLab\CryptoanalysisLab1\CryptoanalysisLab1\prob_0" + Convert.ToString(variant), 1);
+            Console.WriteLine(A.ReadCell(0,19));
+        }
+    }
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||
+    class Excel {
+
+        string path = "";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+
+        public Excel(string path, int Sheet) {
+            
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[Sheet];
+        }
+
+        public string ReadCell(int row, int col) {
+        
+            row++;
+            col++;
+            if (ws.Cells[row, col].Value2 == null)
+                return "";
+            string save = Convert.ToString(ws.Cells[row, col].Value);
+            excel.Quit();
+            return save;
         }
     }
 }
